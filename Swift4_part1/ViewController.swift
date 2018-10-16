1d8debee125979dac04453793908f538a6d141a4
Arun Saxena <admin@admins-macbook-pro.local>
2018-10-16 14:17:54 +0530

    All done.

--- Swift4_part1/ViewController.swift
+++ Swift4_part1/ViewController.swift
@@ -9,12 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let allStudents: [(String, String)] = [("first name","first address"),
+                                           ("second name","second address"),
+                                           ("third name","third address"),
+                                           ("fourth name","fourth address")];
+    var currentStudentIndex : Int = 0;
+
+    @IBOutlet var studentNameLabel: UILabel!
+    @IBOutlet var studentAddressLabel: UILabel!
+    @IBAction func showNextStudent(_ sender: UIButton){
+        currentStudentIndex += 1;
+        if currentStudentIndex == allStudents.count{
+            currentStudentIndex = 0;
+        }
+        showName();
+    }
+    @IBAction func showStudentAddress(_ sender: UIButton){
+        showAddress();
+    }
+
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        showName();
     }
 
+    private func showName(){
+        studentNameLabel.text = allStudents[currentStudentIndex].0;
+        studentAddressLabel.text = "-";
+    }
+    
+    private func showAddress(){
+        studentAddressLabel.text = allStudents[currentStudentIndex].1
+    }
+    
 
 }
+
 
